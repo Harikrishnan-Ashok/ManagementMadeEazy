03bf3406364eaf50e37baf2dd324074c476ae92e
Harikrishnan-Ashok <harikrishnanashokaloor@gmail.com>
2026-03-22 04:38:06 +0000

    base layer

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import AddNewInventoryPage from "./pages/AddNewInventoryPage";
+
+export default function App() {
+
+	return (
+		<>
+			<AddNewInventoryPage />
+		</>
+	)
+}
+
---
+++ client/src/pages/AddNewInventoryPage.jsx
@@ -0,0 +1,92 @@
+import {
+	Box,
+	Button,
+	FormControl,
+	FormLabel,
+	Radio,
+	RadioGroup,
+	FormControlLabel,
+	Typography,
+	Stack,
+	TextField
+} from "@mui/material";
+import { useState } from "react";
+
+export default function AddNewInventoryPage() {
+
+	const [selectedCategory, setSelectedCategory] = useState("tyre");
+
+	const handleAddNewInventoryItemSubmit = (event) => {
+		event.preventDefault();
+
+		const data = new FormData(event.currentTarget);
+		console.log(Object.fromEntries(data));
+	};
+
+	return (
+		<Box
+			display="flex"
+			justifyContent="center"
+			mt={6}
+		>
+			<Box
+				component="form"
+				onSubmit={handleAddNewInventoryItemSubmit}
+				width={400}
+			>
+				<Stack spacing={3}>
+					<Typography variant="h5">
+						Add new inventory item
+					</Typography>
+					<FormControl>
+						<FormLabel>Category</FormLabel>
+						<RadioGroup
+							row
+							name="tyre-category"
+							value={selectedCategory}
+							onChange={(e) => setSelectedCategory(e.target.value)}
+						>
+							<FormControlLabel
+								value="tyre"
+								control={<Radio />}
+								label="Tyre"
+							/>
+							<FormControlLabel
+								value="tube"
+								control={<Radio />}
+								label="Tube"
+							/>
+						</RadioGroup>
+					</FormControl>
+					{selectedCategory === "tyre" && (
+						<FormControl>
+							<FormLabel>Product Description</FormLabel>
+							<RadioGroup
+								row
+								name="tyre-variation"
+							>
+								<FormControlLabel
+									value="tubed"
+									control={<Radio />}
+									label="Tubed"
+								/>
+								<FormControlLabel
+									value="tubeless"
+									control={<Radio />}
+									label="Tubeless"
+								/>
+							</RadioGroup>
+						</FormControl>
+					)}
+					<TextField name="tyre-size" label="size" placeholder={"eg: 165/15 R15"} ></TextField>
+					< Button
+						type="submit"
+						variant="contained"
+					>
+						Add Item
+					</Button>
+				</Stack>
+			</Box>
+		</Box>
+	);
+}
